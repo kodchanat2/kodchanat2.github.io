b9c941ec52025ebfdbae8784ce0f6535d32ba642
kodchanat2 <kodchanat2@gmail.com>
2023-10-03 17:53:25 +0700

    refactor(files): use layouts & pages folder

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-4">
+      <h1 class="text-3xl font-bold">{{ $t('title') }}</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  const { locale, locales: allLocales, setLocale } = useI18n()
+  
+  const locales = computed(() => allLocales.value.filter((l) => l !== locale.value))
+  </script>
