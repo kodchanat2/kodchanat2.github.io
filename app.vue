c882baeed1f59921c679e43983eb0559672459ff
kodchanat2 <kodchanat2@gmail.com>
2023-10-03 14:53:04 +0700

    chore(i18n): init i18n

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>{{ $t('title') }}</h1>
+    <a v-for="l in locales" :key="l.code" @click="() => setLocale(l.code)">
+      {{ l.name }}
+    </a>
   </div>
 </template>
+
+<script setup>
+const { locale, locales: allLocales, setLocale } = useI18n()
+
+const locales = computed(() => allLocales.value.filter((l) => l !== locale.value))
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  // devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n'
+  ],
+  i18n: {
+    locales: [
+      { code: 'en', iso: 'en-US', file: 'en.json', name: 'English' },
+      { code: 'th', iso: 'th-TH', file: 'th.json', name: 'ไทย' }
+    ],
+    defaultLocale: 'en',
+    lazy: true,
+    langDir: 'plugins/locales/',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_configured_locale',
+      fallbackLocale: 'en'
+    },
+  },
 })
