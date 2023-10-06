6f608949cf963bde8589db7f92e1fa40784d0ff2
kodchanat2 <kodchanat2@gmail.com>
2023-10-06 17:31:23 +0700

    fix(tailwind): update color can adjust opacity

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        'text': 'var(--text)',
-        'background': 'var(--background)',
-        'primary': 'var(--primary)',
-        'secondary': 'var(--secondary)',
-        'accent': 'var(--accent)',
-      },
+        'text': 'rgb(var(--text))',
+        'background': 'rgb(var(--background))',
+        'primary': 'rgb(var(--primary))',
+        'secondary': 'rgb(var(--secondary))',
+        'accent': 'rgb(var(--accent))',
+       },       
       height: {
         "screen": "100dvh",
         "screen-small": "100svh",
