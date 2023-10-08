7e7691e74742367092e3dd2ac7bac49602163056
kodchanat2 <kodchanat2@gmail.com>
2023-10-09 03:25:38 +0700

    fix(input): add blank placeholder

--- components/base/Input.vue
+++ components/base/Input.vue
@@ -2,7 +2,7 @@
   <div class="relative w-full">
     <input type="text" :id="id" v-bind="$attrs"
       class="block rounded-lg p-3 pb-2 pt-4 w-full text-text bg-background/90 border-0 border-x-0 border-b-2 border-text/10 appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
-      placeholder="" />
+      placeholder=" " />
     <label :for="id"
       class="absolute text-text/50 duration-300 transform -translate-y-4 scale-75 top-3 z-10 origin-[0] left-3 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">
       {{label}}</label>
