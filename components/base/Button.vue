f1e4a72cc79eccbda01df312cbdb2df06b9fcbc7
kodchanat2 <kodchanat2@gmail.com>
2023-10-12 01:23:15 +0700

    feat(project): show phone with UI

---
+++ components/base/Button.vue
@@ -0,0 +1,6 @@
+<template>
+  <NuxtLink
+    class="p-2 px-4 border border-text text-text rounded-lg font-medium text-base flex justify-center items-center transition-all hover:border-accent hover:text-accent hover:bg-secondary/10 hover:scale-105">
+    <slot />
+  </NuxtLink>
+</template>
--- components/section/project/ProjectCard.vue
+++ components/section/project/ProjectCard.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="w-full mb-16 mt-4 p-4 px-2 rounded-lg relative overflow-hidden shadow-line">
+  <div class="w-full mb-16 mt-4 p-4 px-2 rounded-lg relative overflow-hidden md:overflow-visible">
     <div class="md:hidden absolute z-0 top-0 left-0 w-full h-full flex items-end blur">
       <div class="w-full h-3/4 absolute z-10 top-0 bg-gradient-to-t from-transparent via-background to-background" />
-      <img :src="`images/projects/${project.img}_desktop.jpg`" class="h-3/4 object-cover opacity-70" />
+      <img :src="`images/projects/${project.img}_desktop.webp`" class="h-3/4 object-cover opacity-70" />
     </div>
     <div class="w-full grid grid-cols-1 md:grid-cols-3 relative">
       <div class="flex flex-col items-center justify-around col-span-2 h-full" :class="[swap && 'order-2']">
-        <h3 class="mt-4 text-xl font-semibold flex flex-col items-center">
+        <h3 class="md:w-1/2 mt-4 text-xl font-semibold flex flex-col items-center text-center">
           <span>{{ $t(project.title) }}</span>
-          <span v-if="project.description" class="text-sm font-light">{{ $t(project.description) }}</span>
-          <BaseLink class="mt-4 text-sm flex justify-center items-center" :underline="false" :href="project.link"
+          <span v-if="project.description" class="text-sm font-normal mt-2">{{ $t(project.description) }}</span>
+          <BaseButton class="mt-6" :href="project.link"
             target="_blank">
             {{ $t('view_site') }}
-            <Icon name="line-md:external-link" size="1rem" class="ml-1" />
-          </BaseLink>
+            <Icon name="ep:top-right" size="1.25rem" class="ml-1 translate-x-1" />
+          </BaseButton>
         </h3>
-        <div class="grid grid-cols-4 items-end md:grid-cols-1 mt-8 p-4  rounded-md">
+        <div class="grid grid-cols-4 items-end md:grid-cols-1 mt-6 p-4  rounded-md">
           <div class="relative col-span-3">
-            <img :src="`images/projects/${project.img}_desktop.jpg`"
-              class="absolute top-[4%] left-1/2 -translate-x-1/2 object-cover w-3/4 aspect-[1.5785622239]" />
+            <img :src="`images/projects/${project.img}_desktop.webp`"
+              class="absolute top-[5%] left-1/2 -translate-x-1/2 object-cover w-[74.5%] aspect-[595/372]" />
             <img src="images/desktop.webp" class="relative" />
           </div>
-          <img src="images/phone.webp" class=" mb-auto relative block md:hidden" />
+          <SectionProjectPhone :src="`images/projects/${project.img}_phone.webp`" class="md:hidden scale-110 -translate-x-4 -translate-y-4"/>
         </div>
       </div>
       <div class="hidden md:flex justify-center">
-        <img src="images/phone.webp" />
+        <SectionProjectPhone :src="`images/projects/${project.img}_phone.webp`" :class="swap?'translate-x-10' :'-translate-x-10'"/>
       </div>
     </div>
   </div>
