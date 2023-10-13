663357d7e6dd3e484a3049f79e1b9c0e01f87bea
kodchanat2 <kodchanat2@gmail.com>
2023-10-13 17:15:08 +0700

    Updates

---
+++ _nuxt/i18n.config.b19cdfb1.js
@@ -0,0 +1,1 @@
+import e from"./en.2e12b4e7.js";import a from"./th.14d33716.js";const c=()=>({legacy:!1,locale:"en",fallbackLocale:"en",messages:{en:e,th:a}});export{c as default};
--- assets/config.c90bf88a.js
+++
@@ -1,1 +0,0 @@
-import{Y as p,Z as o,u as i}from"./entry.9ffc3f2c.js";const t={nuxt:{}},f=p(t);function a(){const n=i();return n._appConfig||(n._appConfig=o(f)),n._appConfig}export{a as u};
