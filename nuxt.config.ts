44d3a3dfe0d98cdeeeab6eaeca96c16d26a404f2
kodchanat2 <kodchanat2@gmail.com>
2023-10-13 22:44:23 +0700

    fix(gh): error with _nuxt folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // ssr: false,
+  ssr: false,
+  app: {
+    buildAssetsDir: 'src',
+  },
   // devtools: { enabled: true },
   modules: [
     '@nuxtjs/i18n',
