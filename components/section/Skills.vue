4d7934199d14169445f5b183aab558e7828dfe3b
kodchanat2 <kodchanat2@gmail.com>
2023-10-16 16:32:37 +0700

    feat(skill): add skill icon

--- components/section/Skills.vue
+++ components/section/Skills.vue
@@ -3,7 +3,8 @@
     <div class="w-full max-w-screen-lg mx-auto md:px-2 flex flex-col justify-center">
       <h2 class="mt-4 text-4xl px-4 font-semibold uppercase">{{ $t('skill_title') }}</h2>
 
-      <div class="w-full max-w-full overflow-hidden relative side-fade before:h-0 after:h-0 before:md:h-full after:md:h-full">
+      <div
+        class="w-full max-w-full overflow-hidden relative side-fade before:h-0 after:h-0 before:md:h-full after:md:h-full">
         <div class="w-full overflow-x-auto relative flex snap-x scrollbar-hide">
           <div
             class="snap-center max-w-[calc(100vw-4rem)] w-96 shrink-0 bg-gradient-to-tr from-secondary/25 to-accent/30 shadow-line shadow-secondary/50 border-2 border-secondary rounded-lg mx-2 md:mx-4 my-8 first:md:ml-10 last:mr-20"
@@ -14,11 +15,13 @@
                 <!-- <span v-if="skill.description" class="text-sm font-light">・{{ $t(skill.description) }}</span> -->
               </h3>
               <div class="w-full flex flex-wrap justify-center items-center my-4">
-                <div class="w-full px-2 flex" v-for="item in sortByLevel(skill.list)" :key="item.name">
+                <div class="w-full px-0 flex" v-for="item in sortByLevel(skill.list)" :key="item.name">
                   <p class="w-full">
-                    <span class="text-lg font-medium">{{ item.name }}</span>
+                    <Icon :name="item.icon || `skill-icons:${item.alt}${item.mono ? '' : ('-' + (colorMode.value!=='light'?'dark':'light'))}`"
+                      class="mr-2 mb-1" size="1.2rem" :style="[item.fill && `color:${item.fill}`]" />
+                    <span class="text-lg font-normal">{{ item.name }}</span>
                     <span v-if="item.level" class="text-sm self-center" :class="styleByLevel(item.level)">
-                    ・{{ $t('skill_level_' + [item.level]) }} {{ item.date }}
+                      {{ '・' + $t('skill_level_' + [item.level]) }} {{ item.date }}
                     </span>
                   </p>
                 </div>
@@ -34,12 +37,13 @@
 
 <script setup>
 const { $const } = useNuxtApp();
+const colorMode = useColorMode();
 
 const skills = $const.skills;
 
 
-function styleByLevel(level){
-  switch(Number(level)){
+function styleByLevel(level) {
+  switch (Number(level)) {
     case 9:
       return "text-green-700 font-semibold";
     case 5:
