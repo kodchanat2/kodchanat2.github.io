24f3402f627db6b2b29304f91bdb5187a5bee2b9
kodchanat2 <kodchanat2@gmail.com>
2023-10-19 10:32:25 +0700

    fix(highlight): smooth transition

--- components/navbar/Highlight.vue
+++ components/navbar/Highlight.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col md:flex-row gap-2 relative justify-center items-center pr-2">
-    <div ref="hf" class="absolute hidden md:block rounded-lg bg-primary w-16 h-9 left-1 transition duration-200"></div>
+    <div ref="hf" class="absolute hidden md:block rounded-lg bg-primary w-16 h-9 left-1 transition-all ease-in-out duration-200"></div>
     <div v-for="(item, index) in list" :key="item.name" :id="`nav-${item.name}`" class="w-full p-2 text-center relative text-lg font-normal transition delay-50" :class="route==item.name?'text-accent md:text-background font-semibold':'hidden md:block'">
       <NuxtLink :to="item.to" class="uppercase whitespace-nowrap">
         {{ $t(item.name + '_title') }}
