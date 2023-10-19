89efbc01352da20727bf11e27aec1053d44f0f79
kodchanat2 <kodchanat2@gmail.com>
2023-10-19 11:33:28 +0700

    update(skill): smooth scroll and page cursor

--- components/section/Skills.vue
+++ components/section/Skills.vue
@@ -5,19 +5,20 @@
 
       <div
         class="w-full max-w-full overflow-hidden relative side-fade before:h-0 after:h-0 before:md:h-full after:md:h-full">
-        <div class="w-full overflow-x-auto relative flex snap-x scrollbar-hide">
+        <div ref="scroller" class="w-full overflow-x-auto relative flex snap-x snap-mandatory pl-10 pr-20 scrollbar-hide">
           <div
-            class="snap-center max-w-[calc(100vw-4rem)] w-96 shrink-0 bg-gradient-to-tr from-secondary/25 to-accent/30 shadow-line shadow-secondary/50 border-2 border-secondary rounded-lg mx-2 md:mx-4 my-8 first:md:ml-10 last:mr-20"
+            class="snap-center max-w-[calc(100vw-4rem)] w-96 shrink-0 bg-gradient-to-tr from-secondary/25 to-accent/30 shadow-line shadow-secondary/50 border-2 border-secondary rounded-lg mx-2 md:mx-4 my-8"
             v-for="skill in skills" :key="skill.title">
             <div class="w-full mx-auto px-4 flex flex-col justify-center">
-              <h3 class="mt-4 pb-2 text-xl font-semibold w-full border-b-2 border-accent/30">
+              <h3 class="mt-4 pb-2 text-xl font-semibold text-center md:text-left w-full border-b-2 border-accent/30">
                 <span>{{ $t(skill.title) }}</span>
                 <!-- <span v-if="skill.description" class="text-sm font-light">・{{ $t(skill.description) }}</span> -->
               </h3>
               <div class="w-full flex flex-wrap justify-center items-center my-4">
                 <div class="w-full px-0 flex" v-for="item in sortByLevel(skill.list)" :key="item.name">
                   <p class="w-full">
-                    <Icon :name="item.icon || `skill-icons:${item.alt}${item.mono ? '' : ('-' + (colorMode.value!=='light'?'dark':'light'))}`"
+                    <Icon
+                      :name="item.icon || `skill-icons:${item.alt}${item.mono ? '' : ('-' + (colorMode.value !== 'light' ? 'dark' : 'light'))}`"
                       class="mr-2 mb-1" size="1.2rem" :style="[item.fill && `color:${item.fill}`]" />
                     <span class="text-lg font-normal">{{ item.name }}</span>
                     <span v-if="item.level" class="text-sm self-center" :class="styleByLevel(item.level)">
@@ -29,6 +30,16 @@
             </div>
           </div>
         </div>
+        <button
+          class="absolute top-1 md:top-1/2 translate-y-1/2 translate-x-1/2 md:translate-x-0 md:-translate-y-full left-0 z-[3] rounded-full md:bg-background/80 text-primary p-4"
+          @click="prev">
+          <Icon name="ic:round-arrow-back-ios" size="1.5rem" />
+        </button>
+        <button
+          class="absolute top-1 md:top-1/2 translate-y-1/2 -translate-x-1/2 md:translate-x-0 md:-translate-y-full right-0 z-[3] rounded-full md:bg-background/80 text-primary p-4"
+          @click="next">
+          <Icon name="ic:round-arrow-forward-ios" size="1.5rem" />
+        </button>
       </div>
 
     </div>
@@ -38,6 +49,7 @@
 <script setup>
 const { $const } = useNuxtApp();
 const colorMode = useColorMode();
+const scroller = ref(null);
 
 const skills = $const.skills;
 
@@ -62,5 +74,12 @@ function sortByLevel(array) {
     }
     return b.level - a.level;
   });
+}
+
+const next = () => {
+  scroller.value.scrollBy({ left: 300, behavior: 'smooth' });
+}
+const prev = () => {
+  scroller.value.scrollBy({ left: -300, behavior: 'smooth' });
 }
 </script>
