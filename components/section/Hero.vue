e1423ac55960c2e526be3ee52b2a55299b718bed
kodchanat2 <kodchanat2@gmail.com>
2023-10-25 17:38:47 +0700

    update(SEO): add hidden h1 tag

--- components/section/Hero.vue
+++ components/section/Hero.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="w-full h-screen-large relative overflow-hidden">
     <div class="w-full h-full relative z-[1] flex flex-col justify-around items-center">
-      <div />
+      <div>
+        <h1 style="
+          font-size: 1px;
+          color: #ffffff00;
+          position: absolute;
+          pointer-events: none;
+        ">{{ $t('SEO_title') }}</h1>
+      </div>
       <div class="text-center">
-        <h1 id="hero-title" class="text-5xl sm:text-6xl font-semibold">{{ $t('title') }}</h1>
+        <h2 id="hero-title" class="text-5xl sm:text-6xl font-semibold">{{ $t('title') }}</h2>
         <h2 id="hero-desc" class="mt-4 text-xl font-normal text-primary">
-          {{$rt($tm('alt_title')[0])}}
+          {{ $rt($tm('alt_title')[0]) }}
           <span class="font-semibold">
-            {{$rt($tm('alt_title')[1])}}
+            {{ $rt($tm('alt_title')[1]) }}
           </span>
         </h2>
       </div>
@@ -36,7 +43,7 @@ onMounted(() => {
     ease: 'power4.out',
     delay: 0.5
   })
-  $gsap.to('#hero-title',{
+  $gsap.to('#hero-title', {
     scrollTrigger: {
       trigger: '#hero-title',
       start: '-200% top',
