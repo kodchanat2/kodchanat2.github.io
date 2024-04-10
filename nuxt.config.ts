655310885925fc1ce51e623db03bd4753ee517c8
kodchanat2 <kodchanat2@gmail.com>
2024-04-10 19:56:30 +0700

    feat(env): add clarity env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "jd33997k1e");`
+        })(window, document, "clarity", "script", "${process.env.CLARITY||'jd33997k1e'}");`
         }
       ],
     }
