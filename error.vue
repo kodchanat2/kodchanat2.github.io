670f365807f5a9975eafae8b4b78b26af5cb7a3e
kodchanat2 <kodchanat2@gmail.com>
2024-04-10 18:54:18 +0700

    feat(route): add error page

---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-screen h-screen-large flex justify-center items-center">
+    not found
+  </div>
+</template>
+<script setup>
+// redirect to home
+import { useRouter } from 'vue-router';
+const router = useRouter();
+router.push('/');
+</script>
