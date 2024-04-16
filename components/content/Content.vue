32a4dbd23c8691daa80e167cb43571ced5175341
kodchanat2 <kodchanat2@gmail.com>
2024-04-17 04:58:30 +0700

    fix(content): set max height of image content

--- components/content/Content.vue
+++ components/content/Content.vue
@@ -2,10 +2,10 @@
   <div class="w-full flex flex-col">
     <ContentHeader :data="data" class="px-4" :head="head" :hideButton="hideButton" />
     <div class="w-full h-px bg-text/20 mt-6 mb-2"></div>
-    <div class="w-full p-4 flex flex-col gap-6 items-center font-light">
+    <div class="w-full p-4 flex flex-col gap-4 items-center font-light">
       <div v-for="(ctx,i) in content" :key="i" class="w-full">
         <div v-if="ctx.img" class="w-full flex flex-col items-center">
-          <img :src="imgUrl+'/images/content/'+ctx.img" class="w-full max-w-lg min-h-[100px] bg-secondary shadow-grow shadow-secondary" />
+          <img :src="imgUrl+'/images/content/'+ctx.img" class="w-auto max-w-full md:max-w-lg min-h-[100px] max-h-96 bg-secondary shadow-grow shadow-secondary" />
           <span v-if="ctx.html" v-html="ctx.html" class="font-light text-xs italic text-text/80" />
         </div>
         <span v-else v-html="ctx.html" />
