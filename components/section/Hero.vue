caa1cb68cba78820922e562c30ec180694f889a6
J0HN_lnwza <kodchanat2@gmail.com>
2025-10-03 10:20:15 +0700

    doc(hero): link to work by default

--- components/section/Hero.vue
+++ components/section/Hero.vue
@@ -20,8 +20,8 @@
       </div>
       <NuxtLink
         class="w-fit p-6 px-8 rounded-xl border-2 border-primary text-primary bg-background flex justify-center items-center text-xl font-medium cursor-pointer shadow-line shadow-primary transition-all duration-300 hover:scale-110 hover:shadow-none hover:bg-secondary/70"
-        to="#contact">
-        <Icon name="👋" class="mr-2 animate-pulse" size="2rem" /> {{ $t('button_title') }}
+        to="/browse">
+        {{ $t('button_title') }}
       </NuxtLink>
     </div>
     <div class="absolute top-0 left-0 w-full h-full pointer-events-none hero-bg bg-primary/10" />
